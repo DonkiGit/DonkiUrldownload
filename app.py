9fea840b669864e408c925e9de11413ad82af865
Donkii <shanfarooqi77@gmail.com>
2023-10-02 21:14:30 -0400

    script for url downloading

--- app.py
+++ app.py
@@ -1,22 +1,39 @@
-import requests
-from flask import Flask, request, render_template_string
+from flask import Flask, render_template, request, send_file
+import yt_dlp
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template_string(open('index.html').read())
+    if request.method == "POST":
+        url = request.form["url"]
+        try:
+            ydl_opts = {
+                'format': 'best',
+                'outtmpl': 'downloads/%(title)s.%(ext)s',
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=True)
+                video_path = ydl.prepare_filename(info)
 
-@app.route('/download', methods=['POST'])
-def download():
-    url = request.form['url']
-    response = requests.get(url)
-    filename = url.split("/")[-1]
+                if video_path.endswith(".mp4"):
+                    filename = info['title'] + ".mp4"
 
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+                    response = send_file(
+                        video_path,
+                        as_attachment=True,
+                        download_name=filename,
+                        mimetype='video/mp4'
+                    )
 
-    return f"<a href='{filename}' download>Click here to download {filename}</a>"
+                    return response
+                else:
+                    return "No MP4 video found for this URL."
 
-if __name__ == '__main__':
-    app.run()
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
